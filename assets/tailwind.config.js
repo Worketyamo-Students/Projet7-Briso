c62d61818854e81e8fab7abea00bbbeb514e0040
Briso10 <Kouambrice10@gmail.com>
2024-02-28 15:33:49 +0100

    version10

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -3,6 +3,13 @@
         extend: {
           colors: {
             clifford: '#da373d',
+          },
+          dropShadow: {
+            'md': '0 0 10px #fafaf6c3',
+            '4xl': [
+                '0 35px 35px rgba(0, 0, 0, 0.25)',
+                '0 45px 65px rgba(0, 0, 0, 0.15)'
+            ]
           }
         }
       }
